158ac8cfdd05ad4b45a6b0147a17d20c5192b34e
Vasiliy Rusanov <rvk1986@mail.ru>
2022-11-10 11:13:04 +0300

    finish him

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
+'use strict';
 
+const input = document.querySelector('input');
+const parag = document.querySelector('p');
 
+const debonce = (func, time) => {
+    let timeout;
+
+    return () =>{
+        clearTimeout(timeout);
+        timeout = setTimeout(func, time);
+    };
+};
+
+const getValue = () => {
+    parag.textContent = input.value;
+};
+
+input.addEventListener('input', debonce(getValue, 300));
